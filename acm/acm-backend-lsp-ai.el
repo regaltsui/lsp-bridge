b3fe08acd9c5a5669e4181ab40336361068e2c2f
Regal Tsui <regal@DESKTOP-S4ILN21>
2024-11-06 23:48:33 +0800

    add acm backend code

--- acm/acm-backend-lsp-ai.el
+++ acm/acm-backend-lsp-ai.el
@@ -28,6 +28,32 @@
   :type 'string
   :group 'acm-backend-codeium)
 
+(defvar-local acm-backend-lsp-ai-items nil)
+
+(defun acm-backend-lsp-ai-candidates (keyword)
+  (acm-with-cache-candidates
+   acm-backend-lsp-ai-cache-candiates
+   acm-backend-lsp-ai-items))
+
+(defun acm-backend-lsp-ai-candidate-expand (candidate-info bound-start &optional preview)
+  ;; We need replace whole area with codeium label.
+  (let ((end-position (line-end-position)))
+    (forward-line (- (plist-get candidate-info :line) (count-lines (point-min) (line-beginning-position))))
+    (if preview
+        (acm-preview-create-overlay (point) end-position (plist-get candidate-info :label))
+      (delete-region (point) end-position)
+      (insert (plist-get candidate-info :label))
+      (when acm-backend-lsp-ai-accept
+        (lsp-bridge-call-async
+         "lsp_ai_completion_accept" (plist-get candidate-info :id))))))
+
+(defun acm-backend-lsp-ai-candidate-doc (candidate)
+  (plist-get candidate :documentation))
+
+(defun acm-backend-lsp-ai-clean ()
+  (setq-local acm-backend-lsp-ai-items nil)
+  (setq-local acm-backend-lsp-ai-cache-candiates nil))
+
 
 (provide 'acm-backend-lsp-ai)
 ;;; acm-backend-lsp-ai.el ends here
